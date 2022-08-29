6bb71c9e58cfcb93c7ec948078c6919671ef2cda
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2022-08-29 23:52:23 +0300

    added productsState template

--- Frontend/src/Redux/Store.ts
+++ Frontend/src/Redux/Store.ts
@@ -1,10 +1,11 @@
 import {combineReducers, createStore} from 'redux'
+// import { productsReducer } from "./ProductsState";
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 
-
+// Creating reducers object from all our reducers: 
 // const reducers = combineReducers({
-
+    // productsState: productsReducer 
 // })
 
 // const store = createStore(reducers, composeWithDevTools())
